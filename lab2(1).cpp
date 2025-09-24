59eb0ffac9129565a23e648135fc36b032188fdc
enindaniil07-sudo <enin.daniil07@gmail.com>
2025-09-24 23:19:33 +0200

    Add files via upload

---
+++ lab2(1).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <iomanip> 
+
+int main() {
+    double x;
+    double y;
+    int z;
+
+    std::cout << "First number: ";
+    std::cin >> x;
+
+    std::cout << "Second number: ";
+    std::cin >> y;
+
+    std::cout << "Decimal places: ";
+    std::cin >> z;
+
+    std::cout << std::fixed << std::setprecision(z);
+    std::cout << "Result: " << x*y << std::endl;
+
+    return 0;
+}
