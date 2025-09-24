9c961b5439cf5acf5bfc5ded877064bc4d2f40ff
enindaniil07-sudo <enin.daniil07@gmail.com>
2025-09-24 23:47:39 +0200

    Add files via upload

---
+++ lab2(2).cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+    short A;
+    short B;
+
+    std::cout << "First side: ";
+    std::cin >> A;
+
+    std::cout << "Second side: ";
+    std::cin >> B;
+
+    std::cout << "P = ";
+    std::cout << (A + B)*2 << std::endl;
+    return 0;
+}
